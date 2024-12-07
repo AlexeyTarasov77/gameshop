308b23cd0db4118fc536ae968d57d843cbbfdfaf
AlexeyTarasov77 <alexxey2707@gmail.com>
2024-12-07 13:19:18 +0100

    Returned back db pinging (fixed previous issue with event loop)

--- src/gateways/db/main.py
+++ src/gateways/db/main.py
@@ -1,20 +1,25 @@
+import asyncio
+import logging
+
 from core.utils import Singleton
-from gateways.db.exceptions import AbstractDatabaseExceptionMapper
+from gateways.db.exceptions import AbstractDatabaseExceptionMapper, DBConnectionError
+from psycopg import OperationalError
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
 class SqlAlchemyDatabase(Singleton):
-    # async def ping(self) -> None:
-    #     try:
-    #         async with self._engine.begin() as conn:
-    #             await conn.execute(text("SELECT 1"))
-    #     except OperationalError as e:
-    #         raise DBConnectionError(
-    #             """Connection failed: unable to connect to: '%s' ,
-    #             Make sure your server is available under specified dsn"""
-    #             % self._dsn
-    #         ) from e
+    async def ping(self) -> None:
+        try:
+            async with self._engine.begin() as conn:
+                await conn.execute(text("SELECT 1"))
+        except OperationalError as e:
+            raise DBConnectionError(
+                """Connection failed: unable to connect to: '%s' ,
+                Make sure your storage is available under specified dsn"""
+                % self._dsn
+            ) from e
 
     def __init__(
         self,
@@ -26,3 +31,7 @@ class SqlAlchemyDatabase(Singleton):
         self._engine = create_async_engine(storage_dsn, **engine_params)
         self.session_factory = sessionmaker(self._engine, class_=AsyncSession, expire_on_commit=False)
         self.exception_mapper = exception_mapper
+        try:
+            asyncio.run(self.ping())
+        except RuntimeError as e:
+            logging.error("Failed pinging database due to event loop issue", exc_info=e)
