7d2f94f032b892ca8df5b09e6212b371cd3b9178
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-03-08 18:52:33 +0100

    Created setup redis script which creates indexes

--- src/gateways/redis/main.py
+++ src/gateways/redis/main.py
@@ -1,6 +1,7 @@
 from json import JSONDecoder, JSONEncoder
-from typing import Any
-import redis.asyncio as redis
+from typing import Any, NamedTuple
+from sales import APP_LABEL as SALES_APP_LABEL
+from redis.asyncio import Redis
 from redis.commands.json import JSON
 
 
@@ -12,8 +13,16 @@ class RedisJSONEncoder(JSONEncoder):
             return super().default(o)
 
 
-def init_redis_client(dsn: str) -> redis.Redis:
-    r = redis.from_url(dsn)
+class IndexInfo(NamedTuple):
+    for_prefix: str
+    name: str
+
+
+indexes = {SALES_APP_LABEL: IndexInfo("sales_item:", "idx:sales")}
+
+
+def init_redis_client(dsn: str) -> Redis:
+    r = Redis.from_url(dsn)
     old_json = r.json
 
     def _json_monkeypatch(encoder=RedisJSONEncoder(), decoder=JSONDecoder()) -> JSON:
