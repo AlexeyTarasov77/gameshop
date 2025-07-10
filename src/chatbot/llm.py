545cb64bd5d8bebabd5f99dc36c748dd6f8564b0
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-07-10 14:07:07 +0200

    feat(chatbot): fine-tuned system prompt, create sketch of llm agent

--- src/chatbot/llm.py
+++ src/chatbot/llm.py
@@ -1,5 +1,8 @@
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.language_models import LanguageModelInput
+from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+
+from chatbot.prompts import SYSTEM_PROMPT
 
 
 class LLMAgent:
@@ -7,35 +10,19 @@ class LLMAgent:
         self._llm = ChatGoogleGenerativeAI(
             model="gemini-2.0-flash", google_api_key=google_api_key
         )
-        self._system_prompt = SystemMessage(
-            content="""
-        Ты — дружелюбный и умный помощник на сайте по продаже видеоигр. Твоя цель — помогать пользователям выбирать, искать и приобретать игры, а также ориентироваться по сайту.
-
-        Ты умеешь:
-        - Подсказывать, какие игры подойдут пользователю по жанру, настроению, платформе или популярности.
-        - Давать рекомендации на основе игр, которые уже нравятся пользователю.
-        - Помогать находить игры по ключевым словам.
-        - Объяснять, как оформить покупку, как работает корзина, оплата, возврат.
-        - Отвечать на вопросы о сайте (например, «как создать аккаунт», «как посмотреть свои заказы» и т.п.).
-        - Давать технические советы, если игра не запускается.
-        - Уточнять у пользователя предпочтения, если информации недостаточно.
-
-        Никогда не придумывай игры. Используй только те, которые есть в базе данных (доступ через функцию `find_games`). Если подходящих нет — скажи об этом честно и предложи альтернативу.
-
-        Если пользователь задаёт вопросы не по теме игр или сайта — вежливо объясни, что ты специализируешься на помощи с играми и сайтом.
-
-        Всегда будь дружелюбным, кратким и полезным. Общайся простым и понятным языком, избегай сложных технических терминов без необходимости.
-        """
-        )
+        self._llm = self._llm.bind_tools([])
 
     def _format_user_prompt(self, message: str, user_id: int) -> HumanMessage:
         return HumanMessage(
             content=f"""
-            Reply to user's message and use following user id for tools invocations if needed.
-            If no tools invocations is needed or tool does not require user_id as it's argument - you can simply ignore it.
             user_id: {user_id}
             message: {message}
             """
         )
 
-    # async def reply_to_user(self, message: str, user_id: int) -> str:
+    def _create_llm_user_input(self, message: str, user_id: int) -> LanguageModelInput:
+        return [SYSTEM_PROMPT, self._format_user_prompt(message, user_id)]
+
+    async def reply_to_user(self, message: str, user_id: int) -> str:
+        res = await self._llm.ainvoke(self._create_llm_user_input(message, user_id))
+        return res.text()
